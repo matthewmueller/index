37b98e090f6db128347983bea0fd1f3263b2410d
Matt Mueller <mattmuelle@gmail.com>
2013-01-15 12:59:33 -0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,30 @@
+
+# index
+
+  simple indexing of strings for basic fuzzy search
+
+## Example
+
+
+
+## Installation
+
+    $ component install matthewmueller/index
+
+## API
+
+### Index([words])
+
+Initialize an `Index`, with an optional `words`.
+
+### index.add(words)
+
+Add a string or array of `words` to the index.
+
+### index.search(str)
+
+Search for a string within the index. Returns `true` if the string is present, otherwise `false`.
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+ * Expose `Index`
+ */
+
+module.exports = Index;
+
+/**
+ * Initialize `Index`
+ *
+ * @param {Array} arr
+ * @api public
+ */
+
+function Index(arr) {
+  if(!(this instanceof Index)) return new Index;
+  this.index = '';
+  if(arr) this.add(arr);
+}
+
+/**
+ * Add a term
+ *
+ * @param {Mixed} arr
+ * @return {Index}
+ * @api public
+ */
+
+Index.prototype.add = function(arr) {
+  var index = [];
+  arr = (typeof arr == 'string') ? [ arr ] : arr;
+  for (var i = 0, len = arr.length; i < len; i++)
+    index[index.length] = arr[i].toLowerCase();
+  this.index += index.join('');
+  return this;
+};
+
+/**
+ * Search for a term
+ *
+ * @param {String} str
+ * @return {Boolean} result
+ * @api public
+ */
+
+Index.prototype.search = function(str) {
+  if(~this.index.indexOf(str)) return true;
+  return false;
+};
